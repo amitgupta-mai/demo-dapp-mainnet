178fa60dac137b4b4abde71a8c5d84da30ff58ae
Amit Gupta <amitg.ghy@gmail.com>
2025-04-07 12:16:41 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useMemo } from 'react';
+import { ApiKeySection } from './components/ApiKeySection';
+// import { HeaderActions } from './components/HeaderActions';
+import { useApiKey } from './hooks/useApiKey';
+import { KalpTransfer } from './components/KalpTransfer';
+
+function App() {
+  const { apiKey, isLoading, setApiKey } = useApiKey();
+
+  const defaultDappConfig = useMemo(() => {
+    return {
+      NETWORK_NAME: 'intmainnet',
+      CHAINCODE_NAME: 'koot',
+      CHANNEL_NAME: 'intmainnet',
+      DAPP_URL: 'test.kalp.com',
+      DAPP_ICON:
+        'https://kalp-cbdc-images.s3.ap-south-1.amazonaws.com/CBDC.png',
+      DAPP_NAME: 'sample_dapp',
+      permissions: ['transaction'],
+    };
+  }, []);
+
+  useEffect(() => {
+    if (apiKey) {
+      const renderMyWidget = (window as any).renderMyWidget;
+      if (renderMyWidget) {
+        renderMyWidget('kalp-test', apiKey, defaultDappConfig);
+      } else {
+        console.error('renderMyWidget is not defined');
+      }
+    }
+  }, [apiKey, defaultDappConfig]);
+
+  if (isLoading) {
+    return (
+      <div className='min-h-screen bg-gradient-to-br from-indigo-100 to-purple-100 p-8 flex items-center justify-center'>
+        <div className='text-lg text-gray-600'>Loading...</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-indigo-100 to-purple-100 p-8'>
+      <div className='max-w-md mx-auto'>
+        {!apiKey ? (
+          <ApiKeySection onSubmit={setApiKey} />
+        ) : (
+          <>
+            <div id='kalp-test'></div>
+            {/* <HeaderActions onDisconnect={clearApiKey} /> */}
+            <div className='mt-8'>
+              <KalpTransfer />
+            </div>
+            {/* <TransferSection /> */}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
